3ab0468b9a386e9692a78a533e9f945f9e5179bc
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-30 20:06:00 -0400

    initial recurly commit

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import '@/global.css';
+
+
+export default function RootLayout() {
+  return <Stack/>;
+}
