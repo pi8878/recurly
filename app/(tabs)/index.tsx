942ecb1648407253e354ab020e92d4d1296b6ea1
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-31 16:43:48 -0400

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import "@/global.css"
+import {styled} from "nativewind";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView)
+ 
+export default function App() {
+  return (
+    <SafeAreaView className="flex-1  bg-background p-5">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/onboarding" className="mt-4 px-4  bg-primary text-white rounded">
+        Get Started
+      </Link>
+      <Link href="/(auth)/sign-in" className="mt-4 px-4 bg-primary text-white rounded">
+        Go to Sign In
+      </Link>
+      <Link href="/(auth)/sign-up" className="mt-4 px-4 bg-primary text-white rounded">
+        Go to Sign up
+      </Link>
+
+      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude"},
+        }}
+      >
+        Claude Max Subscription
+      </Link>
+    </SafeAreaView>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,36 @@
 // import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// import { Stack } from 'expo-router';
+// import '@/global.css';
+
+
+// export default function RootLayout() {
+//   return <Stack/>;
+// }
+
+// // export default function RootLayout() {
+// //   return (
+// //     <Stack>
+// //       {/* This is your Landing Page (app/index.tsx) */}
+// //       <Stack.Screen name="index" options={{ headerShown: false }} /> 
+      
+// //       {/* This is your Tab Group (app/(tabs)/) */}
+// //       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      
+// //       {/* Add your other groups/screens here */}
+// //       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+// //     </Stack>
+// //   );
+// }
+
 import { Stack } from 'expo-router';
 import '@/global.css';
 
-
 export default function RootLayout() {
-  return <Stack/>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
